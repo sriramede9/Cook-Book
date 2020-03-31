ecab238edba048cd5634acc940abf5e7125999e0
sriramede9 <only4programming03@gmail.com>
2020-03-31 13:44:15 +0100

    added dropdown functionality using #Directives and #@HostListener ,#@HostBinder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,6 @@ export class AppComponent {
   featureSelectedInHeader: string;
 
   onFeatureSelectedInHeader(feature: string) {
-   
     this.featureSelectedInHeader = feature;
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { Component, OnInit, EventEmitter, Output, Input } from "@angular/core";
 
 @Component({
   selector: "app-header",
@@ -8,11 +8,20 @@ import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 export class HeaderComponent implements OnInit {
   constructor() {}
 
+  dropdownselectedvalue: boolean = false;
+
   ngOnInit(): void {}
 
   @Output() featureSelected = new EventEmitter<string>();
+  @Output() dropdownSelected = new EventEmitter<boolean>();
 
   onSelect(feature: string) {
     this.featureSelected.emit(feature);
+  }
+
+  onDropdownSelected() {
+    this.dropdownselectedvalue = true;
+    this.dropdownSelected.emit(this.dropdownselectedvalue);
+    // console.log(this.dropdownselected);
   }
 }
